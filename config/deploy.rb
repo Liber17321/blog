6e8b7d649dce2d7f78e386b45d3f20d666ca6be6
Liber17321 <tianwh3721@gmail.com>
2018-01-10 15:04:17 +0800

    change cap file

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,3 +40,68 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 # append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
+
+
+namespace :deploy do
+  desc 'create_db'
+  task :create_db do
+    on roles(:app) do
+      within release_path do
+        execute :bundle, :exec, :"rails db:create RAILS_ENV=#{fetch(:stage)}"
+      end
+    end
+  end
+
+  desc 'Uploads required config files'
+  task :upload_configs do
+    on roles(:all) do
+      upload!(".env.#{fetch(:stage)}", "#{deploy_to}/shared/.env")
+    end
+  end
+
+  desc 'Seeds database'
+  task :seed do
+    on roles(:app) do
+      within release_path do
+        execute :bundle, :exec, :"rails db:seed RAILS_ENV=#{fetch(:stage)}"
+      end
+    end
+  end
+
+  before 'deploy:migrate', 'deploy:create_db'
+  after :finished, 'deploy:seed'
+  after :finished, 'app:restart'
+end
+
+namespace :app do
+  desc 'Start application'
+  task :start do
+    on roles(:app) do
+      within "#{fetch(:deploy_to)}/current/" do
+        execute :bundle, :exec, :"puma -C config/puma.rb -e #{fetch(:stage)}"
+      end
+    end
+  end
+
+  desc 'Stop application'
+  task :stop do
+    on roles(:app) do
+      within "#{fetch(:deploy_to)}/current/" do
+        execute :bundle, :exec, :'pumactl -F config/puma.rb stop'
+      end
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app) do
+      within "#{fetch(:deploy_to)}/current/" do
+        if test("[ -f #{deploy_to}/current/tmp/pids/puma.pid ]")
+          execute :bundle, :exec, :'pumactl -F config/puma.rb stop'
+        end
+
+        execute :bundle, :exec, :"puma -C config/puma.rb -e #{fetch(:stage)}"
+      end
+    end
+  end
+end
